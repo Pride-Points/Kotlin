83448cd5f233b459ebe727123aceb3f595a02443
JoaoLimaHeyla <joao.lima@sptech.school>
2024-04-14 14:07:09 -0300

    Adição de pastas e organização

--- PridePoints2/app/src/main/java/scholl/sptech/pridepoints/EnumFragmentos.kt
+++
@@ -1,7 +0,0 @@
-package scholl.sptech.pridepoints
-
-enum class MainFragmentos(val descricao: String) {
-    TELA1("Home"),
-    TELA2("Mapa"),
-    TELA3("Avaliações")
-}
--- PridePoints2/app/src/main/java/scholl/sptech/pridepoints/MenuSelecionado.kt
+++
@@ -1,7 +0,0 @@
-package scholl.sptech.pridepoints
-
-data class MenuItem(
-    val naoSelecionado: Int,
-    val itemSelecionado: Int,
-    val isSelected: Boolean
-)
